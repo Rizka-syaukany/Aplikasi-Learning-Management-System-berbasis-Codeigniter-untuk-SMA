20e11f56eaa27aa28a1cc126d75db670fc3c3469
rizka syaukany <rizka.syaukany@gmail.com>
2021-11-01 19:02:34 +0700

    nyoba fetch

--- app/Views/lms/about.php
+++ app/Views/lms/about.php
@@ -3,7 +3,7 @@
 <?= $this->section('content'); ?>
 <div class="container">
 
-    <h1>halaman about</h1>
+    <h1 class=" bg-gray-200 text-2xl">halaman about</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam officiis eaque veniam ab labore aliquam,
         nulla
         voluptates recusandae culpa quam, omnis nam non iure sequi natus iste eum velit minus?</p>
