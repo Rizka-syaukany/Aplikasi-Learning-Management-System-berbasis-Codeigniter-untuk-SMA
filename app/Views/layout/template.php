cb8ed76961e964173e7dd96ef7c46c555188f048
rizka syaukany <rizka.syaukany@gmail.com>
2021-12-02 21:03:27 +0700

    penggunaan branch coba

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -15,16 +15,14 @@
 </head>
 
 <body class="bg-gradient-to-r from-green-400 to-blue-500 ...">
-    <table>
-        <tr>
 
 
-        </tr>
-        <tr>
-            <?= $this->renderSection('content'); ?>
+    <div>
 
-        </tr>
-    </table>
+        <?= $this->renderSection('content'); ?>
+    </div>
+
+
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
