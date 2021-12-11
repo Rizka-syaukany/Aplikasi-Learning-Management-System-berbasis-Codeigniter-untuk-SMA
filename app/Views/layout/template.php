705ca1e0f23c50bb5aeb0c555a82415488186df4
rizka syaukany <rizka.syaukany@gmail.com>
2021-12-11 23:34:40 +0700

    halaman detail siswa

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -8,6 +8,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="../../../public/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
         integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
