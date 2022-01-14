3625ad72f92c8b649cb0d6d0265ae070e9687e1d
Kanikayudha Tantyo N <66233870+kanugi@users.noreply.github.com>
2022-01-15 06:59:20 +0700

    tampilan form

--- app/Views/lms/login.php
+++ app/Views/lms/login.php
@@ -5,22 +5,21 @@
 
 <div class=" flex flex-col w-4/12 mx-auto shadow-xl bg-white bg-opacity-90 rounded-xl py-6 mt-10 px-10">
     <h1 class="text-center">LOGIN</h1>
-    <img src="/img/logolms.png" alt="" height="400" width="300" class="ml-9">
-    <?php if(session()->getFlashdata('error')) : ?>
-    <div class="alert alert-danger" role="alert">
-        <?= session()->getFlashdata('error'); ?>
-    </div>
+    <img src="/img/logolms.png" alt="" height="400" width="300" class="mx-auto d-block">
+    <br>
+    <?php if (session()->getFlashdata('error')) : ?>
+        <div class="alert alert-danger" role="alert">
+            <?= session()->getFlashdata('error'); ?>
+        </div>
     <?php endif; ?>
     <form action="/auth/v_login" method="POST">
         <div class="flex flex-col mt-4">
-            <label class="text-blue-200-" for="email">Email</label>
-            <input type="email" id="email_user" class="outline-none border-b-2 border-black" name="email_user"
-                placeholder="Masukan Email" required>
+            <label class="text-blue-600 font-semibold text-lg" for="email">Email</label>
+            <input class="w-full px-4 py-2 border rounded-md dark:bg-darker dark:border-gray-700 focus:outline-none focus:ring focus:ring-primary-100 dark:focus:ring-primary-darker" type="email" name="email_user" id="email_user" placeholder="Masukkan Email" required />
         </div>
         <div class="flex flex-col mt-4">
-            <label class="text-blue-200-" for="password">Password</label>
-            <input type="password" id="password" class="outline-none border-b-2 border-gray-600" name="password"
-                placeholder="Masukan Password" Required>
+            <label class="text-blue-600 font-semibold" for="password">Password</label>
+            <input class="w-full px-4 py-2 border rounded-md dark:bg-darker dark:border-gray-700 focus:outline-none focus:ring focus:ring-primary-100 dark:focus:ring-primary-darker" id="password" type="password" name="password" placeholder="Masukkan Password" required />
         </div>
         <!--<button type="submit" class="bg-blue-500 hover:bg-blue-600 text-center py-2 text-white mt-4 ml-20">LOGIN</button>-->
         <button type="submit" class="btn btn-primary mt-4 btn-block text-white">LOGIN</button>
